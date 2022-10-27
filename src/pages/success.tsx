71a8e3d9e06408fe91b9d8c34e48fd29f837a8e9
Welisson Silveira <welissonmwse@gmail.com>
2022-10-27 08:54:04 -0300

    Estrutura visual - Nova pagina de Success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -24,14 +24,22 @@ export default function Success({customerName, product}: SuccessProps) {
 
   return (
     <S.SuccessContainer>
-      <h1>Compra efetuada!</h1>
-
       <S.ImageContainer>
-        <Image src={product.imageUrl} width={120} height={110} alt=""/>
+        <div>
+          <Image src={product.imageUrl} width={120} height={110} alt=""/>
+        </div>
+        <div>
+          <Image src={product.imageUrl} width={120} height={110} alt=""/>
+        </div>
+        <div>
+          <Image src={product.imageUrl} width={120} height={110} alt=""/>
+        </div>
       </S.ImageContainer>
 
+      <h1>Compra efetuada!</h1>
+
       <p>
-        Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> ja está a caminho da sua casa.
+        Uhuul <strong>{customerName}</strong>, sua compra de 2 camisetas ja está a caminho da sua casa.
       </p>
       <Link href="/">
         <a>
--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -11,6 +11,7 @@ export const SuccessContainer = styled('main', {
   h1: {
     fontSize: '$2xl',
     color: '$gray100',
+    marginTop: '4rem',
   },
 
   p: {
@@ -39,18 +40,30 @@ export const SuccessContainer = styled('main', {
 
 export const ImageContainer = styled('div', {
   width: '100%',
-  maxWidth: 130,
-  height: 145,
-  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
-  borderRadius: 8,
-  padding: '0.25rem',
-  marginTop: '4rem',
-
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
 
-  img: {
-    objectFit: 'cover'
-  },
+  div: {
+    width: '100%',
+    maxWidth: 140,
+    height: 140,
+    background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+    boxShadow: '0px 0px 60px rgba(0, 0, 0, 0.8)',
+    borderRadius: "50%",
+    padding: '0.25rem',
+    
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    img: {
+      objectFit: 'cover'
+    },
+
+    '& + div':{
+      marginLeft: -60,
+    }
+  }
 })
